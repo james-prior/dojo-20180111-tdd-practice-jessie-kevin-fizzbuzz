a26a928294f2705bcae5e4c9cb2eee512a2c6319
Jessie and Kevin with Jim looking over their shoulders <klauzau@gmail.com>
2018-01-11 20:48:59 -0500

    tests/test_fizzbuzz.py: Wrote failing test: 7 -> 7

--- tests/test_fizzbuzz.py
+++ tests/test_fizzbuzz.py
@@ -6,6 +6,8 @@ number_to_expected_value = {
     1: 1,
     2: 2,
     5: 'buzz',
+    3: 'fizz',
+    7: 7,
 }
 @pytest.mark.parametrize('number, expected_value', number_to_expected_value.items())
 def test_known_number_returns_expected(number, expected_value):
