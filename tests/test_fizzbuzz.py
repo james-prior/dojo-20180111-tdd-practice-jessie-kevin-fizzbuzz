3300c30bd98cdc9e767a63a3fdd76ebe57dd779c
Jessie and Kevin with Jim looking over their shoulders <klauzau@gmail.com>
2018-01-11 20:13:58 -0500

    tests/test_fizzbuzz.py: Create failing test 1 -> 1

---
+++ tests/test_fizzbuzz.py
@@ -0,0 +1,11 @@
+import pytest
+
+from fizzbuzz import fizzbuzz
+
+number_to_expected_value = {
+    1: 1,
+}
+@pytest.mark.parametrize('number, expected_value', number_to_expected_value.items())
+def test_known_number_returns_expected(number, expected_value):
+    assert expected_value == fizzbuzz(number) 
+
